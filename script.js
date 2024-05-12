a7ee61eeb957cdfcee6a3e8226bafc8839962115
Fairoz-hub <fk143788@gmail.com>
2024-05-13 00:06:11 +0530

    added script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+function generateQRCode(){
+    let userInput = document.getElementById('userInput');
+    // let QRBox = document.getElementById('QRBox');
+    let QRImage = document.getElementById('QRImage');
+
+
+    if(userInput.value === ""){
+        alert("Please enter a value to generate a QR code");
+    }
+    else{
+        QRImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+userInput.value;
+    }
+
+}
